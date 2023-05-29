3291d04b5c79fca11ff56a33b043ac9c2ca84e8a
Jithendramgg <123942557+Jithendramgg@users.noreply.github.com>
2023-05-29 21:04:51 +0530

    Add files via upload

---
+++ programming_assignment_1.py
@@ -0,0 +1,29 @@
+# Question 1. Write a Python program to print "Hello Python"
+print("Hello Python")
+
+#Question 2. Write a Python program to do arithmetical operations addition and division.?
+a= 20
+b= 10
+#Addition
+print("Addition of 10 and 20" , a+b)
+print("Division of 20 and 10" , a/b)
+
+#Question 3. Write a Python program to find the area of a triangle
+height = 20
+base = 20
+print("Area of Triange with height 20 and base 20 is", (20*20)/2)
+
+#Question 4. Write a Python program to swap two variables
+a= 10
+b = 100
+print("before swap data is a = {} and b = {} ".format(a,b))
+temp = a
+a=b
+b=temp
+print("after swap data is a = {} and b = {} ".format(a,b))
+
+#Question 5.Write a Python program to generate a random number
+
+import random 
+print(random.random())
+print(random.randint(1,30))
